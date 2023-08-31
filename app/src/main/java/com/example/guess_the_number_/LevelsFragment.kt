8ee2dd1cc11663464374d5a8c08a2ca7c4744561
Kanat Bayalbayev <Kanatkz07@mail.ru>
2023-08-31 15:20:00 +0600

    created Fragment Manager

--- app/src/main/java/com/example/guess_the_number_/LevelsFragment.kt
+++ app/src/main/java/com/example/guess_the_number_/LevelsFragment.kt
@@ -1,9 +1,32 @@
 package com.example.guess_the_number_
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.guess_the_number_.databinding.LevelsFragmentLayoutBinding
 
 class LevelsFragment : Fragment() {
 
+    private var _binding: LevelsFragmentLayoutBinding? = null
+
+    private val binding: LevelsFragmentLayoutBinding
+        get() = _binding ?: throw RuntimeException("LevelsFragmentLayoutBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = LevelsFragmentLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     companion object {
 
