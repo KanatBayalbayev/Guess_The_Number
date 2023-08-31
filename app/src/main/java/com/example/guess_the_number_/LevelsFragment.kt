9816488a2a7081d0e3a4c1c2e106b22e0f6d7fdf
Kanat Bayalbayev <Kanatkz07@mail.ru>
2023-08-31 15:10:01 +0600

    created Layouts

--- app/src/main/java/com/example/guess_the_number_/LevelsFragment.kt
+++ app/src/main/java/com/example/guess_the_number_/LevelsFragment.kt
@@ -2,6 +2,16 @@ package com.example.guess_the_number_
 
 import androidx.fragment.app.Fragment
 
-class LevelsFragment: Fragment() {
+class LevelsFragment : Fragment() {
+
+
+    companion object {
+
+       const val NAME = "LevelsFragment"
+        fun newInstance(): LevelsFragment {
+            return LevelsFragment()
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/guess_the_number_/StarterFragment.kt
+++ app/src/main/java/com/example/guess_the_number_/StarterFragment.kt
@@ -5,15 +5,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.guess_the_number_.databinding.WelcomeFragmentLayoutBinding
 
-class StarterFragment: Fragment() {
+class StarterFragment : Fragment() {
+
+    private var _binding: WelcomeFragmentLayoutBinding? = null
+
+    private val binding: WelcomeFragmentLayoutBinding
+        get() = _binding ?: throw RuntimeException("WelcomeFragmentLayoutBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = WelcomeFragmentLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonToStartGame.setOnClickListener {
+            launchLevelFragment()
+        }
+    }
+
+    private fun launchLevelFragment() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, LevelsFragment.newInstance())
+            .addToBackStack(LevelsFragment.NAME)
+            .commit()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
